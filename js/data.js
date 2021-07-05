6a3bc54cc3376f3c5dba3aa534cbd3f7f4fa5200
Mac-F01 <83695975+Mac-F01@users.noreply.github.com>
2021-07-05 12:00:33 +0200

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,18 @@
+"use strict"
+let a = 5;
+
+var artikelInfo = [ {
+	name : 'Sprudelwasser',
+	marke : 'Gerolsteiner',
+	hersteller : 'Gerolsteiner Brunnen GmbH & Co. KG',
+	kategorie: 'Nahrungsmittel',
+	stückzahl : '6',
+	beschreibung : 'Bitte nur dieses Wasser kaufen!',
+}, {
+	name : 'AspirinEffekt',
+	marke : 'Aspirin',
+	hersteller : 'Bayer',
+	kategorie: 'Medikament',
+	stückzahl : '20',
+	beschreibung : 'Steht im Medikamentenschrank.',
+} ];
